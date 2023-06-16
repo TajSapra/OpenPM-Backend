9f074253809eac2c1951a544eca9808eff6d5100
Taj Sapra <sapra.taj@live.com>
2023-06-16 16:40:55 +0530

    Local Auth Done

--- Services/Other/verify_and_get_user.js
+++ Services/Other/verify_and_get_user.js
@@ -7,16 +7,9 @@ getuser=async function(req, res){
     if(req.body.secret==undefined&&req.body.password==undefined){
         res.json({error:'Authentication Error. Please try again'})
     }
-    temp_ans={}
-    temp_ans['username']=rows[0]['username']
-    temp_ans['mail']=rows[0]['mail']
-    temp_ans['organisation']=rows[0]['organisation']
-    temp_ans['projects']=rows[0]['projects']
-    temp_ans['completed_tasks']=rows[0]['completed_tasks']
-    temp_ans['assigned_tasks']=rows[0]['assigned_tasks']
     if(req.body.secret==undefined){
         if(req.body.password==rows[0].password){
-            res.json({success : "Updated Successfully", status : 200, user: temp_ans})
+            res.json({success : "Updated Successfully", status : 200})
         }
         else{
             res.json({error:'Authentication Error. Please try again'})
@@ -24,7 +17,7 @@ getuser=async function(req, res){
     }
     else{
         if(req.body.secret==rows[0].user_secret){
-            res.json({success : "Updated Successfully", status : 200, user: temp_ans})
+            res.json({success : "Updated Successfully", status : 200})
         }
         else{
             res.json({error:'Authentication Error. Please try again'})
